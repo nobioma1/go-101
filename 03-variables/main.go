6e4baab55dbbaa6453a6409c0d8d3cafe792bdc6
Noble Obioma <nobioma1@gmail.com>
2020-04-06 13:04:05 +0100

    Single line variable declaration and string interpolation

--- 03-variables/main.go
+++ 03-variables/main.go
@@ -6,5 +6,9 @@ func main() {
 	// explicit variable declaration
 	var port int32
 	port = 3000
-	fmt.Printf("Current port: %d:", port)
+	fmt.Printf("Current port: %d \n", port)
+
+	// One line declaration
+	var speed float32 = 358.69
+	fmt.Printf("It's travelling at %.2f knots\n", speed)
 }
